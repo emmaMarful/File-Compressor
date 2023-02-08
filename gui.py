12e7e2a9e2d7a96b2a6a8c16c4e6cea033cbea95
Emmanuel Marful <eamarful@gmail.com>
2023-02-08 07:21:30 +0000

    user interface for simple file compressor

---
+++ gui.py
@@ -0,0 +1,20 @@
+import PySimpleGUI as user_interface
+
+label1 = user_interface.Text("Select files to compress")
+user_input1 = user_interface.Input()
+choose_btn1 = user_interface.FilesBrowse("Choose")
+
+label2 = user_interface.Text("Select destination folder")
+user_input2 = user_interface.Input()
+choose_btn2 = user_interface.FilesBrowse("Choose")
+
+compress_btn = user_interface.Button("Compress")
+
+layout = [[label1, user_input1, choose_btn1], [label2, user_input2, choose_btn2], [compress_btn]]
+
+windows = user_interface.Window("File Compressor", layout)
+
+windows.read()
+windows.close()
+
+git
