f32dcf34ce9affffb94566ef0d03aa5eb8851d2b
emmaMarful <eamarful@gmail.com>
2023-02-15 23:52:51 +0000

    added a function class for zip conversion

---
+++ zipCreator.py
@@ -0,0 +1,11 @@
+import zipapp
+import  pathlib
+import zipfile
+
+
+def make_archives(filepaths, dest_dir):
+    dest_path = pathlib.Path(dest_dir, "compressed.zip")
+    with zipfile.ZipFile(dest_path, 'w') as archive:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath, arcname=filepath.name)
